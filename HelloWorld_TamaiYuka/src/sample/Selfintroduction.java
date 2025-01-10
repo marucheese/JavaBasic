59ef2ae2e1f07c1a30a3ca231dec764c918890ef
marunoyuuka <marunoyuuka@localhost>
2025-01-10 21:28:03 +0900

    ファーストコミット

---
+++ HelloWorld_TamaiYuka/src/sample/Selfintroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "玉井　優華";
+		// 年齢
+		int age = 27;
+		// 趣味
+		String hobby = "推し活";
+		/*
+		 * 自己紹介を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
