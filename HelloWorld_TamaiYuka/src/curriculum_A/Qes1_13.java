a2112f22058a0b72a09eb9e4833a3f3245e6a1a1
marunoyuuka <marunoyuuka@localhost>
2025-03-10 21:33:47 +0900

    お疲れ様です。問題1_4〜問題1_8の課題です。ご確認お願いいたします。

--- HelloWorld_TamaiYuka/src/curriculum_A/Qes1_13.java
+++ HelloWorld_TamaiYuka/src/curriculum_A/Qes1_13.java
@@ -110,11 +110,6 @@ public class Qes1_13 {
 			System.out.println(age2); //年齢を出力
 			System.out.println(height2); //身長を出力
 			System.out.println(age2 >= 25 || height2 >= 160); // 年齢が25歳以上、または身長が160以上であればtrueを出力
-			
-		
-		
-		
-			
 		}
 		
 	}
