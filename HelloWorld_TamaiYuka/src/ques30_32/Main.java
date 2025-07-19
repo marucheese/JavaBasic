05ade59e224e046678f0032f19356375cd4a961e
marunoyuuka <marunoyuuka@localhost>
2025-07-19 23:58:01 +0900

    お疲れ様です。課題1.31が完了しましたのでご確認お願い致します。

--- HelloWorld_TamaiYuka/src/ques30_32/Main.java
+++ HelloWorld_TamaiYuka/src/ques30_32/Main.java
@@ -1,15 +1,14 @@
 package ques30_32;
 
-public class Main {
-	public static void main(String[] args) {
+class Main {
+	public static void main(String[] argos) {
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60); //問題５ weightの60を引数にいれる
-		
-		System.out.println(person1.name);
-		System.out.println(person1.age);
-		System.out.println(person1.height);
-		
 		person1.print();
-		System.out.println("合計" + Person.getCount() + "人です");
+		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		person2.print();
+		
+		Person.printCount();
+	
 	}
 
 }
--- HelloWorld_TamaiYuka/src/ques30_32/Person.java
+++ HelloWorld_TamaiYuka/src/ques30_32/Person.java
@@ -1,14 +1,14 @@
 package ques30_32;
 
-public class Person {
+class Person {
 	//問題１ インスタンスフィールドを定義
-	String name;
-	int age;
-	double height;
-	double weight;
+	private String name;
+	private int age;
+	private double height;
+	private double weight;
 	
 	//staticフィールド 人数カウント
-	static int count = 0;
+	private static int count = 0;
 	
 	//問題２・３・４ コンストラクタ
 	Person(String name, int age, double height, double weight) {
@@ -20,18 +20,17 @@ public class Person {
 	}
 	
 	//問題８・９ 自己紹介を表示するメソッド
-	double bmi() {
-		return Math.floor(weight/(height*height));
+	public double bmi() {
+		return Math.floor(this.weight / this.height / this.height);
 	}
 	
-	void print() {
+	public void print() {
 		System.out.println("名前は" + this.name + "です");
 		System.out.println("年は" + this.age + "才です");
-		System.out.printf("BMIは%.1fです%n" , this.bmi());
+		System.out.println("BMIは" + String.format("%.1f", this.bmi()) + "です");
+	}
+	public static void printCount() {
+		System.out.println("合計は" + count + "人です");
 	}
 	
-	//問題１０ 合計人数を返すメソッド
-	static int getCount() {
-		return count;
-	}
 }
